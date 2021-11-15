8165aabed66e6edad42a51f15a769ae139c3fc55
Jerry Dimitriou <jd@inteleproc.eu>
2021-11-16 00:22:50 +0200

    more structure in yaml files

--- docker-pdk.ps1
+++ docker-pdk.ps1
@@ -1,4 +1,2 @@
 $ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
-Write-Host "Current script directory is $ScriptDir"
-
 docker run --rm -i -v ${ScriptDir}:/src -p8000:8000 klakegg/pdk:v0.6.0 pdk $args
