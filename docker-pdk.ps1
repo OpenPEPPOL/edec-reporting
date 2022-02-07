9dacf7f01397f35707b3f4592fff3212a283a590
philippe Negro <98015220+PhilippeNegro@users.noreply.github.com>
2022-02-07 16:11:48 +0100

    PNO

--- docker-pdk.ps1
+++ docker-pdk.ps1
@@ -1,2 +1,4 @@
 $ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
-docker run --rm -i -v ${ScriptDir}:/src public.ecr.aws/openpeppol/pdk:0.7.1 pdk $args
+Write-Output $ScriptDir
+#docker run --rm -i -v ${ScriptDir}:/src public.ecr.aws/openpeppol/pdk:0.7.1 pdk $args
+docker run --rm -it -v ${ScriptDir}:/src public.ecr.aws/openpeppol/pdk:0.7.1 /bin/bash
