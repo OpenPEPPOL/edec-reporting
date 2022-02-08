9c9056a0598f8a2342d5fd8068e5f931ee2e7fb8
Jerry Dimitriou <jd@inteleproc.eu>
2022-02-08 17:04:15 +0200

    updated scripts

--- docker-pdk.ps1
+++ docker-pdk.ps1
@@ -1,4 +1,4 @@
 $ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
 Write-Output $ScriptDir
-#docker run --rm -i -v ${ScriptDir}:/src public.ecr.aws/openpeppol/pdk:0.7.1 pdk $args
-docker run --rm -it -v ${ScriptDir}:/src public.ecr.aws/openpeppol/pdk:0.7.1 /bin/bash
+docker run --rm -i -v ${ScriptDir}:/src public.ecr.aws/openpeppol/pdk:0.7.2 pdk $args
+#docker run --rm -it -v ${ScriptDir}:/src public.ecr.aws/openpeppol/pdk:0.7.2 /bin/bash
