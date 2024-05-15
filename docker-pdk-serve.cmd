4a5bfdc14feb35d65280cc828e76ba5c6123d4b6
Philip Helger <philip@helger.com>
2024-05-15 14:11:13 +0200

    Using docker image from AWS

--- docker-pdk-serve.cmd
+++ docker-pdk-serve.cmd
@@ -1,3 +1,4 @@
 @echo off
+aws ecr get-login-password --region eu-west-1 --profile peppol-dev | docker login --username AWS --password-stdin 070318080841.dkr.ecr.eu-west-1.amazonaws.com
 docker stop pdk-serve
 docker run --rm --pull=always -i -d --name pdk-serve -v %CD%:/src -p8000:8000 public.ecr.aws/openpeppol/pdk:latest pdk serve
--- docker-pdk.cmd
+++ docker-pdk.cmd
@@ -1,2 +1,3 @@
 @echo off
-docker run --rm --pull=always -i -v %CD%:/src public.ecr.aws/openpeppol/pdk:latest pdk %*
+aws ecr get-login-password --region eu-west-1 --profile peppol-dev | docker login --username AWS --password-stdin 070318080841.dkr.ecr.eu-west-1.amazonaws.com
+docker run --rm --pull=always -i -v %CD%:/src 070318080841.dkr.ecr.eu-west-1.amazonaws.com/pdk:latest pdk %*
